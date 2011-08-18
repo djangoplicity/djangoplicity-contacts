65d750b8b35651713f978160ec8e9851cb411007
lnielsen_eso <devnull@localhost>
2011-08-18 10:31:43 +0200

    - Added title to admin list view on request from Oana.

--- src/djangoplicity/contacts/admin.py
+++ src/djangoplicity/contacts/admin.py
@@ -88,8 +88,8 @@ class ContactFieldInlineAdmin( admin.TabularInline ):
 	extra = 1
 
 class ContactAdmin( AdminCommentMixin, admin.ModelAdmin ):
-	list_display = ['id', 'last_name', 'first_name', 'organisation', 'department', 'street_1', 'street_2', 'city', 'country', 'email', 'phone', 'website', 'created', 'last_modified' ]
-	list_editable = ['first_name', 'last_name', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', ]
+	list_display = ['id', 'title', 'last_name', 'first_name', 'organisation', 'department', 'street_1', 'street_2', 'city', 'country', 'email', 'phone', 'website', 'created', 'last_modified' ]
+	list_editable = ['title', 'first_name', 'last_name', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', ]
 	list_filter = ['last_modified', 'groups__category__name', 'groups', 'country__groups', 'extra_fields__name', 'country', 'title' ]
 	search_fields = ['first_name', 'last_name', 'title', 'position', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', 'social', ]
 	fieldsets = ( 
