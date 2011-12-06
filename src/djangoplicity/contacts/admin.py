e2690fbd2f7abe4613da1a0c51eea7a08dd740f3
lnielsen_eso <devnull@localhost>
2011-12-06 09:19:51 +0100

    - added position field on request from Oana.

--- src/djangoplicity/contacts/admin.py
+++ src/djangoplicity/contacts/admin.py
@@ -225,8 +225,8 @@ class ContactFieldInlineAdmin( admin.TabularInline ):
 	extra = 1
 
 class ContactAdmin( AdminCommentMixin, admin.ModelAdmin ):
-	list_display = ['id', 'title', 'last_name', 'first_name', 'organisation', 'department', 'tags', 'street_1', 'street_2', 'city', 'country', 'email', 'phone', 'website', 'created', 'last_modified' ]
-	list_editable = ['title', 'first_name', 'last_name', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', ]
+	list_display = ['id', 'title', 'last_name', 'first_name', 'position', 'organisation', 'department', 'tags', 'street_1', 'street_2', 'city', 'country', 'email', 'phone', 'website', 'created', 'last_modified' ]
+	list_editable = ['title', 'first_name', 'last_name', 'position', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', ]
 	list_filter = ['last_modified', 'groups__category__name', 'groups', 'country__groups', 'extra_fields__name', 'country', 'title' ]
 	search_fields = ['first_name', 'last_name', 'title', 'position', 'email', 'organisation', 'department', 'street_1', 'street_2', 'city', 'phone', 'website', 'social', ]
 	fieldsets = ( 
