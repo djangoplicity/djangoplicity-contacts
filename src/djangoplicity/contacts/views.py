5a2b680db90d21a32df9571a2147abcf07f3e972
Mathias Andre <mandre@eso.org>
2014-07-23 10:35:39 +0200

    Add view to allow contacts to be edited without authentication

--- src/djangoplicity/contacts/views.py
+++ src/djangoplicity/contacts/views.py
@@ -1,7 +1,7 @@
 # -*- coding: utf-8 -*-
 #
 # djangoplicity-contacts
-# Copyright (c) 2007-2011, European Southern Observatory (ESO)
+# Copyright (c) 2007-2014, European Southern Observatory (ESO)
 # All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
@@ -14,7 +14,7 @@
 #      notice, this list of conditions and the following disclaimer in the
 #      documentation and/or other materials provided with the distribution.
 #
-#    * Neither the name of the European Southern Observatory nor the names 
+#    * Neither the name of the European Southern Observatory nor the names
 #      of its contributors may be used to endorse or promote products derived
 #      from this software without specific prior written permission.
 #
@@ -28,7 +28,69 @@
 # IN CONTRACT, STRICT LIABILITY, OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE)
 # ARISING IN ANY WAY OUT OF THE USE OF THIS SOFTWARE, EVEN IF ADVISED OF THE
 # POSSIBILITY OF SUCH DAMAGE
-#
+
+from djangoplicity.contacts.models import Contact
+from djangoplicity.contacts.forms import ContactPublicForm
+
+from django.contrib import messages
+from django.contrib.admin.models import LogEntry, CHANGE
+from django.contrib.contenttypes.models import ContentType
+from django.http import Http404
+from django.views.generic import UpdateView
 
 
+class ContactPublicUpdate(UpdateView):
+	'''
+	Class to allow a given contact to be updated without authentication
+	This is meant for people to update their contact information given
+	a unique URL
+	'''
+	model = Contact
+	form_class = ContactPublicForm
+	template_name = 'contacts/contact_public_form.html'
 
+	def get_object(self, queryset=None):
+		pk = self.kwargs.get('uid', None)
+		try:
+			contact = Contact.objects.get(pk=pk)
+		except Contact.DoesNotExist:
+			raise Http404
+
+		return contact
+
+	def form_valid(self, form):
+		'''
+		Add an entry to the Contact's admin history and set
+		a success message to display to the user
+		'''
+		messages.success(self.request, 'Your Contact information have been successfully updated. Thank you!')
+
+		# Get list of modifield fields
+		obj = self.get_object()
+		changed_fields = []
+
+		for field, value in form.cleaned_data.items():
+			if value != getattr(obj, field):
+				changed_fields.append(form[field].label)
+
+		if changed_fields:
+			change_message = 'Changed (from public interface) %s' % ', '.join(changed_fields)
+
+			# log_action expect a valid user_id, as the action is not done by a
+			# registered User we fake it and use 93 (mandre)
+			LogEntry.objects.log_action(
+					user_id=93,
+					content_type_id=ContentType.objects.get_for_model(self.object).pk,
+					object_id=self.object.id,
+					object_repr=unicode(self.object),
+					action_flag=CHANGE,
+					change_message=change_message)
+
+		return super(ContactPublicUpdate, self).form_valid(form)
+
+	def get_success_url(self):
+		'''
+		Return the current path to stay on the same page
+		after a successful edit
+		'''
+		return self.request.path
