3e1de772ee738980179e9055e6499fc467881c63
Edwin Moreno Puentes <edwinmoreno328@gmail.com>
2021-01-28 12:09:08 -0500

    Fix/set dityfields version to 1.4.1 (Compatible with Python2) (#5)

--- setup.py
+++ setup.py
@@ -41,7 +41,7 @@ setup(
     install_requires=[
         'setuptools',
         'django-crispy-forms==1.8.1',
-        'django-dirtyfields',
+        'django-dirtyfields==1.4.1',
         'hashids',
         'xlrd',
         'xlwt',
